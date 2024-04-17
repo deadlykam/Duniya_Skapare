8f45c19d8ca5ed719f42d6d475782f591e6e7542
Kamran Wali <kamran369mars@gmail.com>
2024-04-17 10:03:12 +0100

    Linked the grid tiles together.

--- addons/kamran_wali/duniya_skapare/scripts/grids/ds_grid.gd
+++ addons/kamran_wali/duniya_skapare/scripts/grids/ds_grid.gd
@@ -13,9 +13,11 @@ var _start_tile_type: int
 # Data Properties
 var _data_names: DS_FixedStringArray = load("res://addons/kamran_wali/duniya_skapare/settings/wave_function_collapse_settings/data_names.tres")
 
-
 var _tiles: Array[DS_Tile]
+var _tile_current: DS_Tile
+var _index:= -1
 var _counter1:= -1
+var _counter2:= -1
 var _type_names: String
 
 func _get_property_list():
@@ -47,4 +49,46 @@ func _ready() -> void:
         _tiles.append(DS_Tile.new()) # Adding initiated tiles
         _counter1 += 1
     
-    print(_start_tile_type)
+    # Setting the starting tile
+    _tile_current = _tiles[_index_start_tile]
+    _tile_current.set_type(_start_tile_type)
+
+    _index = 0
+    _counter1 = 0 # Y-axis counter
+
+    while _counter1 < _grid_y: # Loop for going through y-axis tiles
+        _counter2 = 0 # X-axis counter
+        
+        while _counter2 < _grid_x: # Loop for going through x-axis tiles
+
+            # Adding north tile refs
+            _tiles[_index].set_north(
+                _tiles[_index - _grid_y] if
+                _counter1 > 0 #&& (_counter1 <= _grid_y - 1)
+                else null
+            )
+            
+            # Adding sourth tile refs
+            _tiles[_index].set_south(
+                _tiles[_index + _grid_y] if
+                _counter1 < _grid_y - 1
+                else null
+            )
+            
+            # Adding west tile refs
+            _tiles[_index].set_west(
+                _tiles[_index - 1] if
+                _counter2 > 0 #&& (_counter2 <= _grid_x - 1)
+                else null
+            )
+            
+            # Adding east tile refs
+            _tiles[_index].set_east(
+                _tiles[_index + 1] if 
+                _counter2 < _grid_x - 1
+                else null
+            )
+            
+            _index += 1
+            _counter2 += 1
+        _counter1 += 1
