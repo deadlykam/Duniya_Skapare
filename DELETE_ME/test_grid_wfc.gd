075b64f255ede54fc5c41aab8eaf198a7061e553
Kamran Wali <kamran369mars@gmail.com>
2024-04-18 10:17:15 +0100

    Added custom warning for helping users.

--- DELETE_ME/test_grid_wfc.gd
+++ DELETE_ME/test_grid_wfc.gd
@@ -1,14 +1,6 @@
 extends Node
 
 @export var _grid: DS_Grid
-@export var _wfc: DS_WFCGen
-
-var _counter
-var _counter2
-var _tile: DS_Tile
-var _tiles_open: Array[DS_Tile]
-var _tiles_closed: Array[DS_Tile]
-var _tile_types: String
 
 func _ready():
 	_grid.print_grid()
