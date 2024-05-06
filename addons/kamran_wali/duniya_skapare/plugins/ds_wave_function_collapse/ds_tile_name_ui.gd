c7b5cf189574c0d051ba46e3b96137c8b4e93ba9
Kamran Wali <kamran369mars@gmail.com>
2024-05-06 16:25:23 +0100

    Setting the id for the name UIs

--- addons/kamran_wali/duniya_skapare/plugins/ds_wave_function_collapse/ds_tile_name_ui.gd
+++ addons/kamran_wali/duniya_skapare/plugins/ds_wave_function_collapse/ds_tile_name_ui.gd
@@ -1,6 +1,9 @@
 @tool
 extends Control
 
+const DS_WFC_SETTINGS: GDScript = preload("res://addons/kamran_wali/duniya_skapare/plugins/ds_wave_function_collapse/ds_wfc_settings.gd")
+
+var _manager: DS_WFC_SETTINGS
 var _id:= -1
 var _lbl_id: Label
 var _txt_name: LineEdit
@@ -8,12 +11,13 @@ var _txt_name: LineEdit
 func _enter_tree():
     _lbl_id = $Holder/Lbl_ID
     _txt_name = $Holder/Txt_Name
-
-## This method sets the id of the tile name UI.
-func set_id(id:int) -> void:
-    _id = id
     _set_label_id(str(_id))
+
+## This method sets up the tile name UI.
+func setup(manager:DS_WFC_SETTINGS, id:int) -> void:
+    _manager = manager
+    _id = id
 
 ## This method sets the lable id name.
 func _set_label_id(id: String) -> void:
-    _lbl_id.text = "Tile " + id
+    _lbl_id.text = "Tile " + id + ":"
--- addons/kamran_wali/duniya_skapare/plugins/ds_wave_function_collapse/ds_wfc_settings.gd
+++ addons/kamran_wali/duniya_skapare/plugins/ds_wave_function_collapse/ds_wfc_settings.gd
@@ -15,33 +15,37 @@ var _tiles_current:= 1
 var _counter1:= -1
 var _dir_counter:= 0
 var _temp: Node # Needed for instancing objects
+var _counter_id:= 0 # For storing the current id number
 
-func _enter_tree():
+func _enter_tree() -> void:
     _tile_container = $Holder/Tile_Container
     _txt_not = $Holder/NOT_Container/Txt_NoT
     _btn_not_ok = $Holder/NOT_Container/Btn_NOT_OK
 
-func _on_btn_not_ok_pressed():
+func _ready() -> void:
+    _setup() # Setting up the UI at start up
+
+func _on_btn_not_ok_pressed() -> void:
     _number_of_tiles = int(_txt_not.text) # Storing the number of tiles to show.
 
     if _number_of_tiles < _tiles_current: # Condition for removing children
         _counter1 = _tile_container.get_child_count() - 1 # Starting from the farthest child
         while _counter1 >= _number_of_tiles: # Loop for removing children
-            print("Removing children!")
-            _tile_container.get_child(_counter1).queue_free() # Removing the child
+            _remove_tile_input(_counter1)
             _counter1 -= 1
     elif _number_of_tiles > _tiles_current: # Condition for adding children
         _counter1 = 0
         while _counter1 < (_number_of_tiles - _tiles_current): # Loop for adding children
             _temp = _template_tile_name_ui.instantiate()
-            # TODO: Get the tile name script and update the id of the script
+            _temp.setup(self, _counter_id) # Setting the tile name UI
             _tile_container.add_child(_temp)
             _temp = null
+            _counter_id += 1 # Increasing the tile id
             _counter1 += 1
         
     _tiles_current = _number_of_tiles # Updating current tile numbers
 
-func _on_txt_no_t_text_changed(new_text:String):
+func _on_txt_no_t_text_changed(new_text:String) -> void:
     if (new_text.is_valid_int() && new_text != "0" && !new_text.contains("-")
         && !new_text.contains("+")): # Condition for showing the ok button
         _set_font_colour(_txt_not, Color.WHITE)
@@ -49,7 +53,26 @@ func _on_txt_no_t_text_changed(new_text:String):
     else: # Condition for hiding the ok button
         _set_font_colour(_txt_not, Color.RED)
         _btn_not_ok.visible = false
-        
+
+## This method sets up the UI.
+func _setup() -> void:
+    # TODO: Check if more than 1 tile give. If so then NO need to create one extra tile name
+    _add_new_tile_input()
+
+## This method creates a new tile input.
+func _add_new_tile_input() -> void:
+    _temp = _template_tile_name_ui.instantiate()
+    _temp.setup(self, _counter_id) # Setting the tile name UI
+    _tile_container.add_child(_temp)
+    _temp = null
+    _counter_id += 1 # Increasing the tile id
+
+## This method removes the indexth tile input.
+func _remove_tile_input(index:int) -> void:
+    _tile_container.get_child(index).queue_free() # Removing the child
+    _counter_id -= 1 # Decreasing the tile id
+    
+
 ## This method sets the font colour for the control.
 func _set_font_colour(control:Control, colour:Color) -> void:
     control.add_theme_color_override("font_color", colour)
