8f99dadf26f548526ef5158a24143999381e5267
Kamran Wali <kamran369mars@gmail.com>
2024-05-15 16:51:11 +0100

    WFC Algo
    The algo is working but requires a bit more tweaking

--- addons/kamran_wali/duniya_skapare/scripts/stage_generators/ds_wfc_gen.gd
+++ addons/kamran_wali/duniya_skapare/scripts/stage_generators/ds_wfc_gen.gd
@@ -11,15 +11,15 @@ extends DS_BaseGen
 
 var _index_start_tile: int
 var _start_tile_type: int
+@export var _retrace_limit:= 3 # TODO: Make sure it is NOT below 1
 @export var _is_debug: bool
 
 # Properties for internal usage ONLY
 var _tiles_open: Array[DS_Tile]
 var _tiles_closed: Array[DS_Tile]
-var _tiles_failed: Array[DS_Tile]
 var _tile_current: DS_Tile
+var _tile_error: DS_Tile
 var _rules: Array[int] # Final rules
-var _rules_re: Array[int] # Reprocess rules
 var _rng = RandomNumberGenerator.new()
 var _prob:= -1.0
 var _prob_total:= -1.0
@@ -29,8 +29,6 @@ var _temp2: Array[int]
 var _entropy:= -1
 var _type_names: String
 var _grid_pos_names: String
-var _re_type:= -1
-var _re_rot:= -1
 var _c1:= -1
 var _c2:= -1
 var _c_entropy:= -1
@@ -112,14 +110,11 @@ func _setup() -> void:
 			_c1 += 1
 		
 		_tile_current = _tiles_open.pop_at(_c2) # Getting the lowest entropy tile
-		print_rich("[color=green] Tile: ", get_tile_index(_tile_current), "[/color]")
-		print("- Rules: ", _rules)
 		
 		if _tile_current.get_tile_type() == -1: # Checking if tile NOT processed
 			_process_tile(_tile_current, _rules.duplicate()) # Processing the current tile
 			if _tile_current.get_tile_type() == -1: # Condition for storing failed tiles
-				_tiles_failed.append(_tile_current) # Storing the failed tile
-				print_rich("[color=red]- Tile failed: ", get_tile_index(_tile_current), ", Previous closed Tile: ", get_tile_index(_tiles_closed[_tiles_closed.size() - 1]),"[/color]")
+				_process_retrace(0) # Retracing the to fix the error
 		
 		_c1 = 0
 
@@ -134,58 +129,35 @@ func _setup() -> void:
 			_c1 += 1
 		
 		_tiles_closed.append(_tile_current) # The current tile has been processed
-		print_rich("[color=green]===XXX===[/color]")
-		
-	# Condition for showing the debug number of failed tiles found
-	if _is_debug: print("Total Failed Tiles Found: ", _tiles_failed.size())
-	
-	while !_tiles_failed.is_empty(): # Loop for reprocessing failed tiles
-		_tile_current = _tiles_failed.pop_front() # Popping a failed tile to be reprocessed
-
-		_c1 = 0 # Edge index
-		while _c1 < _tile_current.get_edge_size(): # Loop for finding a tile type for the failed tile
-			if _tile_current.get_edge(_c1) != null: # None null tiles to process
-				_re_type = _tile_current.get_edge(_c1).get_tile_type() # Storing the edge's tile type
-				_re_rot = _tile_current.get_edge(_c1).get_tile_rotation_value() # Storing the edge's rot value
-
-				# Rotation fix Mode check
-				if _re_type != -1: # Checking if the tile has a type
-					if _is_found_type(_tile_current.get_edge(_c1), _re_type, _re_rot + 1): # Checking rotational fix
-						_rules = _get_rules(_tile_current) # Getting rules for the failed tile after rotational fix
-						_process_tile(_tile_current, _rules.duplicate()) # Setting the failed tile type
-
-						if _tile_current.get_tile_type() != -1: # Checking if tile type found for the failed tile
-							break # Tile type found, NO further search required for current tile
-				
-				# Other tile type Mode check
-				_rules = _get_rules(_tile_current.get_edge(_c1)) # Getting all the available rules for the adj tile
-				_rules.erase(_re_type) # Removing the already applied tile type
-
-				while !_rules.is_empty(): # Loop to go through all the available rules
-					_tile_current.get_edge(_c1).reset_tile() # Resetting the edge tile for reprocessing
-					_process_tile(_tile_current.get_edge(_c1), _rules.duplicate()) # Reprocessing the edge tile
-
-					if _tile_current.get_edge(_c1).get_tile_type() != -1: # Checking a new tile type found for the edge tile
-						_rules_re = _get_rules(_tile_current) # Getting the rules for the failed tile
-						_process_tile(_tile_current, _rules_re.duplicate()) # Reprocessing the failed tile
-					else:
-						break # No type found from the other tile types
-					
-					if _tile_current.get_tile_type() != -1: # Checking if a type found for the failed tile
-						break
-					else:
-						_rules.erase(_tile_current.get_edge(_c1).get_tile_type()) # Removing the edge's type that failed to fix the current tile
-
-				if _tile_current.get_tile_type() != -1: # Condition for finding a new type for the failed type
-					break # No further search needed, type found for the failed tile
-				else: # Condition for resetting the edge's type and rotation for failing to find a type for the current failed tile
-					_tile_current.get_edge(_c1).set_tile_type(_re_type) # Resetting the edge's type
-					_tile_current.get_edge(_c1).set_tile_rotation_value(_re_rot) # Resetting the edge's rot
-				
-			_c1 += 1
 	
 	# Condition for showing the debug time
 	if _is_debug: print("Total Process Time: ", ((Time.get_unix_time_from_system() - _debug_time) * 1000), "ms")
+
+## This method retraces back to the error tile to fix the error.
+func _process_retrace(current_retrace:int) -> void:
+	if current_retrace == _retrace_limit: # Condition for breaking the recurssion
+		if _is_debug: print("Maximum retrace reached! Stopping retrace process!")
+		return
+
+	# NOTE: After the loop _tile_current will become _tile_error as further call to _is_found_type method
+	#		has the potential to replace the _tile_error value again.
+	while _tile_current != _tile_error && !_tiles_closed.is_empty(): # Loop for finding the error tile
+		_tile_current.reset_tile() # Resetting the tile for reprocessing
+		# TODO: Add the current tile to the open list if NOT in the open list. Do this if some tiles gets left behind after retrace. <--!******
+		_tile_current = _tiles_closed.pop_back() # Popping from the closed tiles and thus making it available for processing
+	
+	# Condition for NOT finding a match after rotational fix
+	if !_is_found_type(_tile_current, _tile_current.get_tile_type(), _tile_current.get_tile_rotation_value() + 1):
+		_rules = _get_rules(_tile_current) # Getting rules for the error tile after failed rotational fix
+		_rules.erase(_tile_current.get_tile_type()) # Removing the already applied tile type
+
+		_tile_current.reset_tile() # Resetting the tile for reprocessing
+		_process_tile(_tile_current, _rules.duplicate()) # Reprocessing to find a new tile type for the error tile
+
+		if _tile_current.get_tile_type() == -1: # Checking if a new tile type is NOT found
+			current_retrace += 1
+			_process_retrace(current_retrace) # Calling retrace process again as tile error NOT fixed
+
 
 ## This method gets the available rules for a tile but also using one edge of the tile
 ## as a temp value.
@@ -225,6 +197,7 @@ func _get_rules(tile:DS_Tile) -> Array[int]:
 ## This method process the tile.
 func _process_tile(tile: DS_Tile, rules: Array[int]) -> void:
 	while rules.size() != 0:
+		_tile_error = null # Resetting the error tile at the new tile selection
 		_prob = _rng.randf() # Getting probability value
 		_prob_total = (1.0 / rules.size()) # Resetting to find the tile type
 		_c_process1 = 0 # Acts as rule index
@@ -259,6 +232,7 @@ func _is_found_type(tile:DS_Tile, type:int, rot:int) -> bool:
 
 					# Condition for NOT finding any matches so breaking the loop for the next check
 					if !_temp_rules.has(tile.get_edge(_c_found1).get_tile_type()):
+						_tile_error = tile.get_edge(_c_found1) # Storing the tile that caused error
 						break
 					else: # Condtion to check if neighbour allows to set the tile
 						# Storing the neighbour's edge rules toward the current tile
@@ -273,6 +247,7 @@ func _is_found_type(tile:DS_Tile, type:int, rot:int) -> bool:
 						)
 
 						if !_temp_rules.has(type): # NO matches found from the neighbour's edge
+							_tile_error = tile.get_edge(_c_found1) # Storing the tile that caused error
 							break
 				else: # Neighbour tile has NOT been set
 					# Condition to check if current tile rotation does NOT allows neighbour tile to have
